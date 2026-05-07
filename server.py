0e2085812bc00c22de46c7fbe50eb6e23e6b0043
supportnrirealtor-design <support.nrirealtor@gmail.com>
2026-05-07 16:49:06 +0530

    Update server.py

--- server.py
+++ server.py
@@ -29,6 +29,12 @@ client = AsyncIOMotorClient(
     serverSelectionTimeoutMS=30000
 )
 db = client[db_name]
+cloudinary.config(
+    cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.environ.get("CLOUDINARY_API_KEY"),
+    api_secret=os.environ.get("CLOUDINARY_API_SECRET"),
+    secure=True
+)
 
 # Object Storage
 STORAGE_URL = "https://integrations.emergentagent.com/objstore/api/v1/storage"
