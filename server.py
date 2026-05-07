efc399e62f3402456dc286e0bf199eba3a7448e4
supportnrirealtor-design <support.nrirealtor@gmail.com>
2026-05-07 09:09:10 +0530

    Update server.py

--- server.py
+++ server.py
@@ -18,7 +18,12 @@ load_dotenv(ROOT_DIR / '.env')
 # MongoDB connection
 mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
 db_name = os.environ.get('DB_NAME', 'nri_realtor')
-client = AsyncIOMotorClient(mongo_url)
+client = AsyncIOMotorClient(
+    mongo_url,
+    tls=True,
+    tlsAllowInvalidCertificates=True,
+    serverSelectionTimeoutMS=30000
+)
 db = client[db_name]
 
 # Object Storage
